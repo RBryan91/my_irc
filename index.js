d6e047f3be2df72d59ee701c45260f5c8dd89240
Renaudin Bryan <bryan.renaudin@epitech.eu>
2022-10-28 10:46:47 +0200

    Ajout des personne co

--- index.js
+++ index.js
@@ -4,21 +4,39 @@ const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
-
+const id = [];
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/login.html');
-  });
-app.get('/index.html', (req, res) => {
   res.sendFile(__dirname + '/index.html');
+ 
 });
 
 io.on('connection', (socket) => {
+  
+  socket.on("tableau",(inpu)=>{
+    
+    console.log(inpu)
+    let tempo = new Object()
+    tempo.name = inpu
+    tempo.id = socket.id
+    id.push(tempo)
+   // id[inpu] = socket.id
+    console.log(id)
+    io.emit("tableauremplis", id)
+
+    console.log(id)
+  }) 
+  
+
+
   console.log('a user connected');
+  socket.join("Briiiaaaaa");
+  
+  
   socket.on('chat message', (msg) => {
     io.emit('chat message', msg);
-    
   });
+
   socket.on('disconnect', () => {
     console.log('user disconnected');
   });
